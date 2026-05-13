125ee3309f36d212f05c149dcb17e874cd319d7b
Jonathan <33232878+unlikelykoala@users.noreply.github.com>
2026-05-13 16:19:12 -0400

    Working dumb agent

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -6,13 +6,15 @@ import { DuckDBStore } from "@mastra/duckdb";
 import { MastraCompositeStore } from '@mastra/core/storage';
 import { Observability, MastraStorageExporter, MastraPlatformExporter, SensitiveDataFilter } from '@mastra/observability';
 import { weatherWorkflow } from './workflows/weather-workflow';
+import { helloWorldAgent } from './agents/oiva1-agent';
 import { weatherAgent } from './agents/weather-agent';
 import { toolCallAppropriatenessScorer, completenessScorer, translationScorer } from './scorers/weather-scorer';
 
+
 export const mastra = new Mastra({
-  workflows: { weatherWorkflow },
-  agents: { weatherAgent },
-  scorers: { toolCallAppropriatenessScorer, completenessScorer, translationScorer },
+  workflows: {},
+  agents: { helloWorldAgent, weatherAgent },
+  scorers: {},
   storage: new MastraCompositeStore({
     id: 'composite-storage',
     default: new LibSQLStore({
