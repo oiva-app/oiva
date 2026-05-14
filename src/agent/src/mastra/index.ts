6d84a54c737a63525363a46c7025bc90c63ebc23
SValenziano <senorvalenz@gmail.com>
2026-05-14 13:22:29 -0400

    feat: impelement OTelExporter.  Data is being successfully ingested into Honeycomb via the Exporter.
    Not sure yet: how to view data in Honeycomb "Agent Timeline"?  https://docs.honeycomb.io/investigate/observe/agent-timeline

--- src/agent/src/mastra/index.ts
+++ src/agent/src/mastra/index.ts
@@ -5,11 +5,12 @@ import { LibSQLStore } from '@mastra/libsql';
 import { DuckDBStore } from "@mastra/duckdb";
 import { MastraCompositeStore } from '@mastra/core/storage';
 import { Observability, MastraStorageExporter, MastraPlatformExporter, SensitiveDataFilter } from '@mastra/observability';
+import { OtelExporter } from '@mastra/otel-exporter'
 import { weatherWorkflow } from './workflows/weather-workflow';
 import { helloWorldAgent } from './agents/oiva1-agent';
 import { weatherAgent } from './agents/weather-agent';
 import { toolCallAppropriatenessScorer, completenessScorer, translationScorer } from './scorers/weather-scorer';
-
+import { env } from "./config/env"
 
 export const mastra = new Mastra({
   workflows: {},
@@ -36,6 +37,14 @@ export const mastra = new Mastra({
         exporters: [
           new MastraStorageExporter(), // Persists observability events to Mastra Storage
           new MastraPlatformExporter(), // Sends observability events to Mastra Platform (if MASTRA_PLATFORM_ACCESS_TOKEN is set)
+          new OtelExporter({
+            provider: {
+              custom: {
+                endpoint: env.COLLECTOR_ENDPOINT,  
+                protocol: 'http'
+              }
+            }
+          })
         ],
         spanOutputProcessors: [
           new SensitiveDataFilter(), // Redacts sensitive data like passwords, tokens, keys
