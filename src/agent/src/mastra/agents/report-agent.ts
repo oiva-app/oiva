9775bef996c2d58736943ac94c10b70c0a311fb7
tiiarikama <tiiarikama@hotmail.com>
2026-05-24 18:37:01 -0700

    refactor: cleans up unnecessary configuration from report agent definition

--- src/agent/src/mastra/agents/report-agent.ts
+++ src/agent/src/mastra/agents/report-agent.ts
@@ -1,16 +1,10 @@
 import { Agent } from "@mastra/core/agent";
 import { prompt } from "../prompts/system-prompt-report-agent";
 import { Memory } from "@mastra/memory";
-import { env } from "../config/env"
-
 
 export const reportAgent = new Agent({
   id: "report-agent",
   name: "Report Agent",
   instructions: prompt,
-  memory: new Memory(),
   model: "openai/gpt-5.4",
-  defaultOptions: {
-    maxSteps: env.REPORT_MAX_STEPS,
-  },
 });
